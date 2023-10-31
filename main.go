a8842508a32317ec870df01d64754eff79c1e5d7
yang <3264085417@qq.com>
2023-10-31 16:46:09 +0800

    fix: roll back

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"AILN/app/config"
+	"AILN/app/routers"
+	"github.com/gin-gonic/gin"
+	"log"
+)
 
 func main() {
-	//config.Info = config.New("./config/config.yaml")
-	//engine := gin.Default()
-	//routers.Load(engine)
-	//if err := engine.Run("0.0.0.0:8080"); err != nil {
-	//	log.Fatal(err)
-	//}
-	for {
-		fmt.Println("Starting!")
+	config.Info = config.New("./config/config.yaml")
+	engine := gin.Default()
+	routers.Load(engine)
+	if err := engine.Run("0.0.0.0:8080"); err != nil {
+		log.Fatal(err)
 	}
 }
