8ceac9d121ab492c7ef19e06d27dba4fd3051506
yang <3264085417@qq.com>
2023-10-31 15:31:42 +0800

    fix: main --> null

--- main.go
+++ main.go
@@ -1,17 +1,15 @@
 package main
 
-import (
-	"AILN/app/config"
-	"AILN/app/routers"
-	"github.com/gin-gonic/gin"
-	"log"
-)
+import "fmt"
 
 func main() {
-	config.Info = config.New("./config/config.yaml")
-	engine := gin.Default()
-	routers.Load(engine)
-	if err := engine.Run("0.0.0.0:8080"); err != nil {
-		log.Fatal(err)
+	//config.Info = config.New("./config/config.yaml")
+	//engine := gin.Default()
+	//routers.Load(engine)
+	//if err := engine.Run("0.0.0.0:8080"); err != nil {
+	//	log.Fatal(err)
+	//}
+	for {
+		fmt.Println("Starting!")
 	}
 }
